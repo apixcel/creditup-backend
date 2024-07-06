d00bea16d58d4b2a0022f0e1c10bf1f72e75277f
sakibsarkar <sakibsarkar707@gmail.com>
2024-07-06 13:08:23 +0600

    env update

--- build/routes/googleApi.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.googleApiRoutes = void 0;
-const express_1 = require("express");
-const googleApi_controller_1 = require("../controller/googleApi.controller");
-const router = (0, express_1.Router)();
-router.get("/read", googleApi_controller_1.readSheetController);
-router.delete("/delete/:rowIndex", googleApi_controller_1.deleteRowFromSheetController);
-exports.googleApiRoutes = router;
--- build/validation/appendSheetDataValidation.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sheetAppentDataValidationSchema = void 0;
-const zod_1 = require("zod");
-exports.sheetAppentDataValidationSchema = zod_1.z.object({
-    data: zod_1.z.tuple([zod_1.z.string(), zod_1.z.number(), zod_1.z.number()]),
-});
